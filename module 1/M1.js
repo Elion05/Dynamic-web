fdb71c4a593630ea2975a7618da2221e85c753d5
Elion05 <elion.rexborn@gmail.com>
2025-02-14 13:32:27 +0100

    Elion

--- module 1/M1.js
+++ module 1/M1.js
@@ -8,11 +8,11 @@ specialElement.forEach(element => {
 
 //onderlijnt de 'p' op de juiste plaats
 let pElement = document.querySelectorAll('p');
-pElement[1].style.textDecoration = 'underline';
+pElement[1].style.textDecoration = 'underline'
 //toont aantal special elementen in de html
 let output = document.getElementById('output');
 output.textContent = `Aantal special elementen : ${specialElement.length}`;
 
 //alternatieve methode met de getElementByClassName
-let specialElementAlt = document.getElementsByClassName('special');
-console.log(`Gevonden met getElementByClasseName : ${specialElementAlt.length}`);
+//let specialElementAlt = document.getElementsByClassName('special');
+//console.log(`Gevonden met getElementByClasseName : ${specialElementAlt.length}`);
