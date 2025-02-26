839febf58dcc944c27f3d7a55c94f11b24d4b1f5
Elion05 <elion.rexborn@gmail.com>
2025-02-26 20:12:12 +0100

    commit

--- module 3/M2/M2.js
+++ module 3/M2/M2.js
@@ -1,17 +1,14 @@
 "use strict";
 
+
+
 const spelerNaam = document.getElementById('playerName');
 const spelerScore = document.getElementById('score');
 const scoreBord = document.getElementById('scoreBoard');
 
-scoreBord.addEventListener('click', ()=>{
-    if(!spelerNaam||!spelerScore){
-        alert('Vul alles in. ');
-    };
-});
-
 function voegScoreToe(){
-    document.getElementById('scoreBoard').innerHTML =
-
-    `Speler Stats: <li>${spelerNaam} + ${spelerScore}</li>`
+if( !spelerNaam.value || isNaN(spelerScore.value)){
+alert('Vul alles in');
+return;
+    };
 };
